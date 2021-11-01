d02280796c9c9103bb6565340ee57aa2779daae7
PigeonSensei <parkour5813@gmail.com>
2021-11-01 17:57:22 +0900

    update

---
+++ include/scan_to_point_cloud2.h
@@ -0,0 +1,86 @@
+#ifndef SCAN_TO_POINT_CLOUD2_H
+#define SCAN_TO_POINT_CLOUD2_H
+
+#include <ros/ros.h>
+
+#include <sensor_msgs/LaserScan.h>
+#include <sensor_msgs/PointCloud2.h>
+
+#include <tf/tf.h>
+#include <tf/transform_listener.h>
+
+#include "laser_geometry/laser_geometry.h"
+
+class ScanToPointCloud2
+{
+public:
+    ScanToPointCloud2(ros::NodeHandle &n)
+       {
+          // open run
+          GetRosParam();
+          InitRos(n);
+       }
+       ~ScanToPointCloud2()
+       {
+
+       }
+
+    struct Check
+    {
+      uint32_t count = 0;
+      uint32_t previous_count = 0;
+      uint32_t check_count = 0;
+    };
+
+    void GetRosParam();
+
+    void InitRos(ros::NodeHandle &n);
+
+    void ScanCallBack(const sensor_msgs::LaserScan &data);
+
+    bool CheckCallback(Check *check,uint32_t *seq);
+
+    sensor_msgs::PointCloud2 _ScanToPointCloud2(sensor_msgs::LaserScan &data);
+
+    void UpdatePointCloud2();
+
+    void Update();
+
+    void Publisher();
+
+    void Spin();
+
+private:
+
+    struct CallBackDatas
+    {
+      sensor_msgs::LaserScan scan;
+    };
+
+    struct InitDatas
+    {
+      sensor_msgs::LaserScan scan;
+    };
+
+    struct TopicNames
+    {
+      std::string scan;
+      std::string scan_to_point_cloud2;
+    };
+
+    CallBackDatas callback_datas_;
+    InitDatas init_datas_;
+    Check sequence_check_scan_;
+    TopicNames topic_names_;
+
+    ros::Subscriber subscriber_scan_;
+
+    ros::Publisher publisher_scan_to_point_cloud2_;
+
+    tf::TransformListener tf_listener_;
+    laser_geometry::LaserProjection laser_projector_;
+    sensor_msgs::PointCloud2 point_cloud2_;
+
+};
+
+#endif // SCAN_TO_POINT_CLOUD2_H
---
+++ src/scan_to_point_cloud2.cpp
@@ -0,0 +1,111 @@
+#include "scan_to_point_cloud2.h"
+
+void ScanToPointCloud2::ScanCallBack(const sensor_msgs::LaserScan &data)
+{
+  callback_datas_.scan = data;
+}
+
+bool ScanToPointCloud2::CheckCallback(Check *check, uint32_t *seq)
+{
+    check->count = *seq;
+
+    if(check->count == check->previous_count)
+    {
+        check->check_count++;
+
+        if(check->check_count > 99)
+        {
+            check->check_count = 100;
+            return false;
+        }
+
+    }
+    else
+    {
+        check->check_count= 0;
+    }
+
+    check->previous_count = check->count;
+
+    return true;
+}
+
+sensor_msgs::PointCloud2 ScanToPointCloud2::_ScanToPointCloud2(sensor_msgs::LaserScan &data)
+{
+  sensor_msgs::PointCloud2 point_cloud2;
+
+  ros::Time now_time = ros::Time::now();
+  point_cloud2.header.stamp = now_time;
+  data.header.stamp = now_time;
+
+  if(!tf_listener_.waitForTransform(data.header.frame_id,data.header.frame_id,data.header.stamp,ros::Duration(1.0)))
+  {
+    return point_cloud2;
+  }
+
+  laser_projector_.transformLaserScanToPointCloud(data.header.frame_id,data,
+          point_cloud2,tf_listener_);
+
+  return point_cloud2;
+}
+
+void ScanToPointCloud2::GetRosParam()
+{
+    ros::NodeHandle n("~");
+    n.param<std::string>("TopicNameScan", topic_names_.scan, "scan");
+    n.param<std::string>("TopicNameScanToPointCloud2", topic_names_.scan_to_point_cloud2, "scan_to_point_cloud2");
+}
+
+void ScanToPointCloud2::InitRos(ros::NodeHandle &n)
+{
+    subscriber_scan_ = n.subscribe(topic_names_.scan, 1, &ScanToPointCloud2::ScanCallBack, this);
+    publisher_scan_to_point_cloud2_ = n.advertise<sensor_msgs::PointCloud2>(topic_names_.scan_to_point_cloud2,1000);
+}
+
+void ScanToPointCloud2::UpdatePointCloud2()
+{
+  point_cloud2_ = _ScanToPointCloud2(callback_datas_.scan);
+}
+
+void ScanToPointCloud2::Update()
+{
+  UpdatePointCloud2();
+}
+
+void ScanToPointCloud2::Publisher()
+{
+  publisher_scan_to_point_cloud2_.publish(point_cloud2_);
+}
+
+void ScanToPointCloud2::Spin()
+{
+  Update();
+  if(CheckCallback(&sequence_check_scan_, &callback_datas_.scan.header.seq) == false)
+  {
+      callback_datas_.scan = init_datas_.scan;
+  }
+  Publisher();
+}
+
+int main(int argc, char **argv)
+{
+  ros::init(argc, argv, "scan_to_point_cloud2_node");
+  ros::NodeHandle n;
+
+  ros::Rate loop_rate(60);
+
+  ScanToPointCloud2 scan_to_point_cloud2(n);
+
+  ROS_INFO("scan_to_point_cloud2_node Open");
+
+  while (ros::ok())
+  {
+    scan_to_point_cloud2.Spin();
+    ros::spinOnce();
+    loop_rate.sleep();
+  }
+
+  ROS_INFO("scan_to_point_cloud2_node Close");
+
+  return 0;
+}
